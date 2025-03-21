3e35d21691702c20f964d328fb852a24e9b58b8f
weckid <162923739+weckid@users.noreply.github.com>
2025-03-21 12:59:13 +0300

    add header, routes, route and fixed mistakes

--- dental-front/src/App.jsx
+++ dental-front/src/App.jsx
@@ -1,16 +1,14 @@
-import {Routes, Route} from "react-router-dom";
-import { Header } from "./components/Header/Header";
-import  MainContent from "./components/Main/MainContent/MainContent"
-
-
+import {Header} from "./components/Header/Header"
+import { MainContent } from "./components/Main/MainContent/MainContent";
+import { Routes, Route } from "react-router-dom";
 function App() {
 
    return (
     <>
-     <Header/>
-     <Routes>
-      <Route path="/MainContent" element={<MainContent/>}></Route>
-     </Routes>
+      <Header/>
+      <Routes>
+        <Route path="/MainContent" element={<MainContent/>}></Route>
+      </Routes>
     </>
   );
 }
--- dental-front/src/components/Header/Header.jsx
+++ dental-front/src/components/Header/Header.jsx
@@ -3,34 +3,34 @@ import "./HeaderStyle.css";
 import { Link, useNavigate } from "react-router-dom";
 import { observer } from "mobx-react-lite";
 
-export const Header = observer (() => {
-    const [isMenuOpen, setMenuOpen] = useState(false);
-    const navigate = useNavigate();
+export const Header = observer(() => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const navigate = useNavigate();
 
-    const toggleMenu = () => {
-        setMenuOpen(!isMenuOpen);
-    };
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
 
-    return (
+  return (
     <header className="header">
-       <div className="logo">
+      <div className="logo">
         <Link to="/MainContent">
-        <img src="/logo.png" alt="Logo" />
+          <img src="/logo.png" alt="Logo" />
         </Link>
-       </div>
-       <nav className="nav">
+      </div>
+      <nav className="nav">
         <ul>
           <Link to="/About">О нас</Link>
-          <Link to ="/Contacts">Контакты</Link>
-          <Link to ="/Entry">Запись</Link>
-          <Link to ="/Catalog">Каталог услуг</Link>
+          <Link to="/Contacts">Контакты</Link>
+          <Link to="/Entry">Запись</Link>
+          <Link to="/Catalog">Каталог услуг</Link>
         </ul>
-        <div className="nav_img">
-          <Link to="/Profile">
-          <img src="/customer.png" alt="" />
-          </Link>
-        </div>
-       </nav>
+      </nav>
+      <div className="nav_img">
+        <Link to="/Profile">
+          <img src="/customer.png" alt="Profile" />
+        </Link>
+      </div>
     </header>
-    );
+  );
 });
